be44db8405b5052b41370e2f5f1e21024134d8ef
Quentin Burgniard <quentinburgniard@gmail.com>
2020-09-27 19:59:50 +0200

    feat(aws): add amazon services

---
+++ config/middleware.js
@@ -0,0 +1,37 @@
+var middleware = {
+  settings: {
+    cron: {
+      enabled: true
+    },
+    favicon: {
+      path: 'favicon.png'
+    },
+    gzip: {
+      enabled: true
+    },
+    poweredBy: {
+      enabled: true
+    }
+  }
+};
+
+if (process.env.ENV === 'production') {
+  middleware.settings.cors = {
+    enabled: true,
+    origin: 'https://quentinburgniard.com'
+  };
+  middleware.settings.csp = {
+    enabled: true,
+    policy: "default-src 'self' *.digitalleman.com"
+  };
+  middleware.settings.logger = {
+    level: 'warn'
+  };
+  middleware.settings.hsts = {
+    enabled: true
+  };
+} else {
+
+};
+
+module.exports = middleware;
---
+++ config/plugins.js
@@ -0,0 +1,25 @@
+module.exports = ({ env }) => ({
+  email: {
+    provider: 'amazon-ses',
+    providerOptions: {
+      key: env('AWS_USER'),
+      secret: env('AWS_PASSWORD'),
+      amazon: 'https://email.eu-central-1.amazonaws.com',
+    },
+    settings: {
+      defaultFrom: 'api@digitalleman.com',
+      defaultReplyTo: 'contact@digitalleman.com',
+    },
+  },
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_USER'),
+      secretAccessKey: env('AWS_PASSWORD'),
+      region: 'eu-central-1',
+      params: {
+        Bucket: 'api.quentinburgniard.com',
+      },
+    },
+  },
+});
